edfd7d1e5d15f90016264b3b950244ca55ee2d26
omur8819 <omur8819@gmail.com>
2020-12-12 13:43:21 +0300

    Grid marjin editted

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 import './App.css';
+import FullWidthGrid from './Components/GridSurface/GridSurface';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+
+export default function App() {
+  const classes = useStyles();
+
+  const [bookData, setbookData] = useState([]);
+
+
+  const fetchBooks = () => {
+    axios.get("https://api.nytimes.com/svc/topstories/v2/books.json?api-key=7hfOFGALLGSAwSOPuN3R2ClK1XtuMLhG")
+
+    
+    .then(response => {
+      setbookData(response.data.results)
+      
+    })
+    .catch(err=> {
+      console.log(err)
+    })
+  };
+
+  useEffect(()=> {
+    fetchBooks()
+  }, [])
+
+  console.log(bookData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={1} justify="center" >
+        <Grid item xs={12} sm={12}>
+            Book Publication
+        </Grid>
+        {bookData.map( d =>
+          <FullWidthGrid data={d}/>
+        )}
+      </Grid>
     </div>
   );
 }
 
-export default App;
+
